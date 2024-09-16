61e7c3727da13940802b23a14d2e0a90779f89cc
Saranya Selvaraj <saranya.professional7@gmail.com>
2024-09-16 13:22:11 +1000

    basic javascript project with jest

---
+++ jest-javascript/src/main.test.ts
@@ -0,0 +1,9 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { beforeEach, describe, expect, test } from 'vitest';
+
+test('should set correct initial starting point [0, 0]', () => {
+    expect(true).toBeTruthy();
+})
